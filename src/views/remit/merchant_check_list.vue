db21352d99eb798a5b7077db35bcb03547a85325
bootmall <bootmall@gmail.com>
2018-09-28 19:18:39 +0800

    优化商户审核出款

--- src/views/remit/merchant_check_list.vue
+++ src/views/remit/merchant_check_list.vue
@@ -32,9 +32,14 @@
         </el-option>
       </el-select>
 
-      <el-button class="filter-item" size="small" type="primary" v-waves icon="search" @click="handleFilter">搜索</el-button>
-      <el-button class="filter-item" size="small" type="success" v-waves icon="search" @click="handleShowUpdateStatus(1,'')">批量审核通过</el-button>
-      <el-button class="filter-item" size="small" type="danger" v-waves icon="search" @click="handleShowUpdateStatus(2,'')">批量拒绝并退款</el-button>
+      <el-button class="filter-item" size="small" type="primary" v-waves icon="search" @click="handleFilter">搜索
+      </el-button>
+      <el-button class="filter-item" size="small" type="success" v-waves icon="search"
+                 @click="handleShowUpdateStatus(1,'')">批量审核通过
+      </el-button>
+      <el-button class="filter-item" size="small" type="danger" v-waves icon="search"
+                 @click="handleShowUpdateStatus(2,'')">批量拒绝并退款
+      </el-button>
     </div>
 
     <el-table :key='tableKey' :data="list" v-loading="listLoading" element-loading-text="数据加载中，请稍候..." border fit
@@ -55,8 +60,12 @@
       <el-table-column prop="created_at" label="建立时间"></el-table-column>
       <el-table-column fixed="right" width="160" align="center" label="操作" class-name="op-column">
         <template slot-scope="scope">
-          <el-button class="filter-item" size="small" type="success" v-waves icon="search" @click="handleShowUpdateStatus(1,scope.row.id)">通过</el-button>
-          <el-button class="filter-item" size="small" type="danger" v-waves icon="search" @click="handleShowUpdateStatus(2,scope.row.id)">拒绝</el-button>
+          <el-button class="filter-item" size="small" type="success" v-waves icon="search"
+                     @click="handleShowUpdateStatus(1,scope.row.id)">通过
+          </el-button>
+          <el-button class="filter-item" size="small" type="danger" v-waves icon="search"
+                     @click="handleShowUpdateStatus(2,scope.row.id)">拒绝
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -97,227 +106,236 @@
 </template>
 
 <script>
-  import waves from '@/directive/waves/index.js' // 水波纹指令
-  import {parseTime} from '@/utils'
-  import common from '@/utils/common'
-  import axios from '@/utils/http'
-  import {mapGetters} from 'vuex'
+    import waves from '@/directive/waves/index.js' // 水波纹指令
+    import {parseTime} from '@/utils'
+    import common from '@/utils/common'
+    import axios from '@/utils/http'
+    import {mapGetters} from 'vuex'
 
-  export default {
-    name: 'vue_merchant_check_remit',
-    directives: {
-      waves
-    },
-    components: {},
-    data() {
-      return {
-        rules: {},
-        list: null,
-        total: null,
-        listLoading: true,
-        listQuery: {
-          page: 1,
-          limit: 20,
-          importance: undefined,
-          dateStart: new Date(new Date().setHours(0, 0, 0, 0)),//.getDateStr(-3),
-          dateEnd: null,
-          orderNo: null,
-          merchantOrderNo: null,
-          status: null,
-          bankAccount: null,
-          bankNo: null,
-          merchantNo: null,
-          merchantAccount: null,
-          sort: '',
-          selfCheck:1,
+    export default {
+        name: 'vue_merchant_check_remit',
+        directives: {
+            waves
         },
-        summery: {
-          'amount': 0,
+        components: {},
+        data() {
+            return {
+                rules: {},
+                list: null,
+                total: null,
+                listLoading: true,
+                listQuery: {
+                    page: 1,
+                    limit: 20,
+                    importance: undefined,
+                    dateStart: new Date(new Date().setHours(0, 0, 0, 0)),//.getDateStr(-3),
+                    dateEnd: null,
+                    orderNo: null,
+                    merchantOrderNo: null,
+                    status: null,
+                    bankAccount: null,
+                    bankNo: null,
+                    merchantNo: null,
+                    merchantAccount: null,
+                    sort: '',
+                    selfCheck: 1,
+                },
+                summery: {
+                    'amount': 0,
+                },
+                tableKey: 0,
+                constFalse: false,
+                constTrue: true,
+                statusOptions: [],
+                multipleSelection: [],
+                checkVisible: false,
+                canCheckRemitStatus: 0,
+                checkLoading: false,
+                checkForm: {
+                    'status': '',
+                    't2fa': '',
+                    'pwd': '',
+                },
+                checkFormAlert: '',
+                pickerOptions: {
+                    disabledDate(time) {
+                        return time.getTime() > Date.now();
+                    },
+                    shortcuts: [{
+                        text: '今天',
+                        onClick(picker) {
+                            picker.$emit('pick', new Date());
+                        }
+                    }, {
+                        text: '昨天',
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setTime(date.getTime() - 3600 * 1000 * 24);
+                            picker.$emit('pick', date);
+                        }
+                    }, {
+                        text: '一周前',
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit('pick', date);
+                        }
+                    }]
+                },
+            }
         },
-        tableKey: 0,
-        constFalse: false,
-        constTrue: true,
-        statusOptions: [],
-        multipleSelection:[],
-        checkVisible:false,
-        canCheckRemitStatus:0,
-        checkLoading:false,
-        checkForm:{
-          'status':'',
-          't2fa':'',
-          'pwd':'',
+        filters: {},
+        created() {
+            this.getList()
         },
-        checkFormAlert:'',
-        pickerOptions: {
-          disabledDate(time) {
-            return time.getTime() > Date.now();
-          },
-          shortcuts: [{
-            text: '今天',
-            onClick(picker) {
-              picker.$emit('pick', new Date());
-            }
-          }, {
-            text: '昨天',
-            onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24);
-              picker.$emit('pick', date);
-            }
-          }, {
-            text: '一周前',
-            onClick(picker) {
-              const date = new Date();
-              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
-              picker.$emit('pick', date);
-            }
-          }]
+        mounted() {
+            // console.log('mounted',this.$route.path)
         },
-      }
-    },
-    filters: {},
-    created() {
-      this.getList()
-    },
-    mounted() {
-      // console.log('mounted',this.$route.path)
-    },
-    updated() {
+        updated() {
 
-    },
-    computed: {
-      ...mapGetters([
-        'roles', 'uid', 'user'
-      ])
-    },
-    methods: {
-      getList() {
-        var self = this
+        },
+        computed: {
+            ...mapGetters([
+                'roles', 'uid', 'user'
+            ])
+        },
+        methods: {
+            getList() {
+                var self = this
 
-        self.listLoading = true
-        for (let i in self.listQuery) {
-          if (self.listQuery[i] == '__ALL__') self.listQuery[i] = ''
-        }
-        axios.post('/remit/my-list', self.listQuery).then(
-          res => {
-            self.listLoading = false
+                self.listLoading = true
+                for (let i in self.listQuery) {
+                    if (self.listQuery[i] == '__ALL__') self.listQuery[i] = ''
+                }
+                axios.post('/remit/my-list', self.listQuery).then(
+                    res => {
+                        self.listLoading = false
 
-            if (res.code != 0) {
-              self.$message.error({message: res.message})
-            } else {
-              self.canCheckRemitStatus = res.data.canCheckRemitStatus
-              self.list = res.data.data
-              self.summery = res.data.summery
-              self.total = res.data.pagination.total
-              self.statusOptions = res.data.condition.statusOptions
-              self.channelAccountOptions = res.data.condition.channelAccountOptions
+                        if (res.code != 0) {
+                            self.$message.error({message: res.message})
+                        } else {
+                            self.canCheckRemitStatus = res.data.canCheckRemitStatus
+                            self.list = res.data.data
+                            self.summery = res.data.summery
+                            self.total = res.data.pagination.total
+                            self.statusOptions = res.data.condition.statusOptions
+                            self.channelAccountOptions = res.data.condition.channelAccountOptions
+                        }
+
+                    },
+                    res => {
+                        self.listLoading = false
+                        self.$message.error({message: res.message})
+                    }
+                )
+            },
+            handleShowUpdateStatus(status, id) {
+                let self = this
+                self.checkForm.status = status
+                self.checkForm.remitIdList = self.multipleSelection
+                if (id) {
+                    self.checkForm.remitIdList = [id]
+                }
+                let msg = '通过审核'
+                if (status == 2) {
+                    msg = '拒绝提交并退款'
+                }
+                self.checkFormAlert = "您将要把结算订单设置为: " + msg
+                self.checkVisible = true
+            },
+            handleUpdateStatus() {
+                self = this
+                self.checkLoading = true
+                if (!self.checkForm.remitIdList || self.checkForm.remitIdList.length == 0) {
+                    this.$alert('订单选择失败,请从新选择订单', '失败提示', {
+                        confirmButtonText: '确定',
+                        callback: action => {
+
+                        }
+                    });
+                    return;
+                }
+                axios.post('/remit/check', self.checkForm).then(
+                    res => {
+                        self.checkLoading = false
+                        if (res.code != 0) {
+                            self.$message.error({message: res.message})
+                        } else {
+                            self.$message.success({message: res.message})
+                            self.checkVisible = false
+                            self.getList();
+                            self.checkForm.t2fa = ''
+                            self.checkForm.pwd = ''
+                        }
+                    },
+                    res => {
+                        self.$message.error({message: res.message})
+                    }
+                )
+
+            },
+            showNotifyRet(row) {
+                // self.$message.error({message: res.message})
+                this.$alert(row.notify_ret, '商户服务器响应内容', {
+                    confirmButtonText: '确定',
+                    callback: action => {
+
+                    }
+                });
+            },
+            getSummaries(param) {
+                const {columns, data} = param;
+                const sums = [];
+                columns.forEach((column, index) => {
+                    if (index === 0) {
+                        sums[index] = '总计';
+                        return;
+                    }
+                    sums[index] = 'N/A';
+                });
+                sums[3] = this.summery.amount + '元'
+                return sums;
+            },
+            handleFilter() {
+                this.listQuery.page = 1
+                this.getList()
+            },
+            handleSizeChange(val) {
+                this.listQuery.limit = val
+                this.getList()
+            },
+            handleCurrentChange(val) {
+                this.listQuery.page = val
+                this.getList()
+            },
+            timeFilter(time) {
+                if (!time[0]) {
+                    this.listQuery.start = undefined
+                    this.listQuery.end = undefined
+                    return
+                }
+                this.listQuery.start = parseInt(+time[0] / 1000)
+                this.listQuery.end = parseInt((+time[1] + 3600 * 1000 * 24) / 1000)
+            },
+            handleSelectionChange(val) {
+                this.multipleSelection = []
+                for (let v of val) {
+                    this.multipleSelection.push(v.id)
+                }
+            },
+            checkNumber: function () {
+                // console.log(this.listQuery.minMoney);
+                let reg = /^([1-9][\d]{0,7}|0)(\.[\d]{1,2})?$/;
+                if (!reg.test(this.listQuery.minMoney)) {
+                    this.listQuery.minMoney = '';
+                }
+                if (!reg.test(this.listQuery.maxMoney)) {
+                    this.listQuery.maxMoney = '';
+                }
             }
+        }
 
-          },
-          res => {
-            self.listLoading = false
-            self.$message.error({message: res.message})
-          }
-        )
-      },
-      handleShowUpdateStatus(status,id){
-        let self = this
-        self.checkForm.status = status
-        self.checkForm.remitIdList =  self.multipleSelection
-        if(id){
-          self.checkForm.remitIdList = [id]
-        }
-        let msg = '通过审核'
-        if(status==2){
-          msg = '拒绝提交并退款'
-        }
-        self.checkFormAlert = "您将要把结算订单设置为: "+msg
-        self.checkVisible = true
-      },
-      handleUpdateStatus() {
-        self = this
-        self.checkLoading = true
-        axios.post('/remit/check', self.checkForm).then(
-          res => {
-            self.checkLoading = false
-            if (res.code != 0) {
-              self.$message.error({message: res.message})
-            } else {
-              self.$message.success({message: res.message})
-              self.checkVisible = false
-              self.getList();
-              self.checkForm.t2fa = ''
-              self.checkForm.pwd = ''
-            }
-          },
-          res => {
-            self.$message.error({message: res.message})
-          }
-        )
-
-      },
-      showNotifyRet(row) {
-        // self.$message.error({message: res.message})
-        this.$alert(row.notify_ret, '商户服务器响应内容', {
-          confirmButtonText: '确定',
-          callback: action => {
-
-          }
-        });
-      },
-      getSummaries(param) {
-        const {columns, data} = param;
-        const sums = [];
-        columns.forEach((column, index) => {
-          if (index === 0) {
-            sums[index] = '总计';
-            return;
-          }
-          sums[index] = 'N/A';
-        });
-        sums[3] = this.summery.amount + '元'
-        return sums;
-      },
-      handleFilter() {
-        this.listQuery.page = 1
-        this.getList()
-      },
-      handleSizeChange(val) {
-        this.listQuery.limit = val
-        this.getList()
-      },
-      handleCurrentChange(val) {
-        this.listQuery.page = val
-        this.getList()
-      },
-      timeFilter(time) {
-        if (!time[0]) {
-          this.listQuery.start = undefined
-          this.listQuery.end = undefined
-          return
-        }
-        this.listQuery.start = parseInt(+time[0] / 1000)
-        this.listQuery.end = parseInt((+time[1] + 3600 * 1000 * 24) / 1000)
-      },
-      handleSelectionChange(val) {
-        this.multipleSelection = []
-        for(let v of val){
-          this.multipleSelection.push(v.id)
-        }
-      },
-      checkNumber: function () {
-        // console.log(this.listQuery.minMoney);
-        let reg = /^([1-9][\d]{0,7}|0)(\.[\d]{1,2})?$/;
-        if (!reg.test(this.listQuery.minMoney)) {
-          this.listQuery.minMoney = '';
-        }
-        if (!reg.test(this.listQuery.maxMoney)) {
-          this.listQuery.maxMoney = '';
-        }
-      }
     }
-
-  }
 </script>
 
 <style>
