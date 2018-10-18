0d3c670d6afdb0087ce35cb20dda060c64de753d
bootmall <bootmall@gmail.com>
2018-10-18 22:46:02 +0800

    优化渠道余额显示

--- src/views/admin/channel/balance.vue
+++ src/views/admin/channel/balance.vue
@@ -7,9 +7,17 @@
         <ul class="list">
             <li v-for="item in list" :key="item.id">
                 <span class="c-name">{{item.channel_name}}</span>:
-                <span class="c-num">{{item.balance|toFixed}}/{{item.frozen_balance|toFixed}}</span>
+                <span class="c-num">{{item.total_balance|toFixed}}(<span style="color: green">{{item.balance|toFixed}}</span>/<span style="color: orangered">{{item.frozen_balance|toFixed}}</span>)</span>
             </li>
         </ul>
+        <el-row :gutter="20">
+            <el-col :span="4" :offset="16"><div class="grid-content grid-content-label bg-purple">渠道可用总余额:</div></el-col>
+            <el-col :span="4"><div class="grid-content grid-content-value bg-purple">{{channel_total_available_balance}}</div></el-col>
+        </el-row>
+        <el-row :gutter="20">
+            <el-col :span="4" :offset="16"><div class="grid-content grid-content-label bg-purple">渠道冻结总余额:</div></el-col>
+            <el-col :span="4"><div class="grid-content grid-content-value bg-purple">{{channel_total_frozen_balance}}</div></el-col>
+        </el-row>
         <el-row :gutter="20">
             <el-col :span="4" :offset="16"><div class="grid-content grid-content-label bg-purple">渠道总余额:</div></el-col>
             <el-col :span="4"><div class="grid-content grid-content-value bg-purple">{{channel_total_balance}}</div></el-col>
@@ -77,6 +85,8 @@
             if (res.code == 0) {
               self.list = res.data.list
               self.channel_total_balance = parseFloat(res.data.channel_total_balance).toFixed(2)
+              self.channel_total_available_balance = parseFloat(res.data.channel_total_available_balance).toFixed(2)
+              self.channel_total_frozen_balance = parseFloat(res.data.channel_total_frozen_balance).toFixed(2)
               self.merchant_total_balance = parseFloat(res.data.merchant_total_balance.balance).toFixed(2)
               self.merchant_total_frozen_balance = parseFloat(res.data.merchant_total_balance.frozen_balance).toFixed(2)
               self.total_profit = res.data.total_profit
