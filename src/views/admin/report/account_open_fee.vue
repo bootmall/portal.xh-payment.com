214d763f54871e175b6b35c3b5ada81213f4eb04
kerr8819 <kerr8819@gmail.com>
2018-10-18 21:03:51 +0800

    调整开户费统计页面

--- src/views/admin/report/account_open_fee.vue
+++ src/views/admin/report/account_open_fee.vue
@@ -1,49 +1,68 @@
 <template>
     <div class="app-container calendar-list-container">
         <div class="filter-container">
-            <el-date-picker class="filter-item"
-                            v-model="listQuery.dateStart"
-                            align="right"
-                            type="date"
-                            placeholder="开始日期"
-                            size="small"
-                            style="width: 200px;"
-                            :picker-options="pickerOptions">
-            </el-date-picker>
-            <el-date-picker class="filter-item"
-                            v-model="listQuery.dateEnd"
-                            align="right"
-                            type="date"
-                            size="small"
-                            style="width: 200px;"
-                            placeholder="结束日期"
-                            :picker-options="pickerOptions">
-            </el-date-picker>
+            <!--<el-date-picker class="filter-item"-->
+                            <!--v-model="listQuery.dateStart"-->
+                            <!--align="right"-->
+                            <!--type="date"-->
+                            <!--placeholder="开始日期"-->
+                            <!--size="small"-->
+                            <!--style="width: 200px;"-->
+                            <!--:picker-options="pickerOptions">-->
+            <!--</el-date-picker>-->
+            <!--<el-date-picker class="filter-item"-->
+                            <!--v-model="listQuery.dateEnd"-->
+                            <!--align="right"-->
+                            <!--type="date"-->
+                            <!--size="small"-->
+                            <!--style="width: 200px;"-->
+                            <!--placeholder="结束日期"-->
+                            <!--:picker-options="pickerOptions">-->
+            <!--</el-date-picker>-->
+            <el-select class="filter-item" v-model="listQuery.month" placeholder="月份" filterable>
+                <el-option
+                        v-for="(item,key) in monthOptions"
+                        :key="key"
+                        :label="item"
+                        :value="key">
+                </el-option>
+            </el-select>
             <el-button class="filter-item"  size="small" type="primary" icon="el-icon-search" @click="handleFilter">搜索</el-button>
         </div>
         <el-table :key='tableKey' :data="list" v-loading="listLoading" element-loading-text="数据加载中，请稍候..." border fit highlight-current-row style="width: 100%">
-            <el-table-column label="商户ID" prop="user_id"></el-table-column>
-            <el-table-column label="商户名" prop="username"></el-table-column>
-            <el-table-column label="上级代理" prop="parent_agent_name"></el-table-column>
-            <el-table-column label="状态" prop="status_str"></el-table-column>
-            <el-table-column label="开户费金额" prop="fee"></el-table-column>
-            <el-table-column label="实际缴纳金额" prop="fee_paid"></el-table-column>
-            <el-table-column label="缴纳时间" prop="paid_at"></el-table-column>
-            <el-table-column label="缴纳订单号" prop="order_no"></el-table-column>
-            <el-table-column label="订单生成时间" prop="order_created_at"></el-table-column>
-            <el-table-column label="创建时间" prop="created_at"></el-table-column>
+            <el-table-column label="商户ID" align="center" prop="merchant_id"></el-table-column>
+            <el-table-column label="商户名" align="center" prop="username"></el-table-column>
+            <el-table-column label="开户数" align="center" prop="total"></el-table-column>
+            <el-table-column label="开户费金额" align="center" prop="fee"></el-table-column>
+            <el-table-column label="实际缴纳金额" align="center" prop="fee_paid"></el-table-column>
+            <el-table-column label="操作" align="center">
+                <template slot-scope="scope">
+                    <el-button @click.native="showInfo(scope.row)"  type="primary" size="mini">详情</el-button>
+                </template>
+            </el-table-column>
         </el-table>
-        <el-row>
-            <el-tag type="warning" v-for="(item,key) in openFeeTotal" :key="key">
-                <span style="color: blue;font-weight: bold">{{item.name}}</span>:
-                <span style="color: red;font-weight: bold">{{item.open_fee}}</span>
-            </el-tag>
-        </el-row>
-        <div v-show="!listLoading" class="pagination-container">
-            <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="listQuery.page"
-                           :page-sizes="[20,30, 50,100]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total">
-            </el-pagination>
-        </div>
+        <el-dialog title="开户费明细" :visible.sync="infoVisible" width="70%">
+            <el-table :key='tableKey' :data="infoList" v-loading="listLoading" element-loading-text="数据加载中，请稍候..." border fit highlight-current-row style="width: 100%">
+                <el-table-column label="商户ID" prop="user_id"></el-table-column>
+                <el-table-column label="商户名" prop="username"></el-table-column>
+                <el-table-column label="上级代理" prop="parent_agent_name"></el-table-column>
+                <el-table-column label="状态" prop="status_str"></el-table-column>
+                <el-table-column label="开户费金额" prop="fee"></el-table-column>
+                <el-table-column label="实际缴纳金额" prop="fee_paid"></el-table-column>
+                <el-table-column label="缴纳时间" prop="paid_at"></el-table-column>
+                <el-table-column label="缴纳订单号" prop="order_no"></el-table-column>
+                <el-table-column label="订单生成时间" prop="order_created_at"></el-table-column>
+                <el-table-column label="创建时间" prop="created_at"></el-table-column>
+            </el-table>
+            <div v-show="!listLoading" class="pagination-container">
+                <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="listQuery.page"
+                               :page-sizes="[20,30, 50,100]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total">
+                </el-pagination>
+            </div>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="infoVisible = false">关 闭</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -60,8 +79,11 @@
                 listQuery: {
                     page: 1,
                     limit: 20,
-                    dateStart: new Date(new Date().setDate(new Date().getDate()-30)),
-                    dateEnd: new Date(),
+                    month:new Date().getMonth(),
+                    agent_merchant_id:null,
+                    agent_merchant_name:null,
+                    // dateStart: new Date(new Date().setDate(new Date().getDate()-30)),
+                    // dateEnd: new Date(),
                 },
                 pickerOptions: {
                     disabledDate(time) {
@@ -69,6 +91,9 @@
                     }
                 },
                 openFeeTotal:null,
+                monthOptions:['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+                infoVisible:false,
+                infoList:null,
             }
         },
         created() {
@@ -78,15 +103,13 @@
             getList() {
                 var self = this
                 self.listLoading = true
-                axios.post('/admin/report/account-open-fee', self.listQuery).then(
+                axios.post('/admin/report/account-open-fee', {month:self.listQuery.month}).then(
                     res => {
                         self.listLoading = false
                         if(res.code != 0 ){
                             self.$message.error({message:res.message})
                         }else{
-                            self.list = res.data.list
-                            self.openFeeTotal = res.data.parentTotal
-                            self.total = res.data.total
+                            self.list = res.data
                         }
                     }
                 )
@@ -101,6 +124,25 @@
             handleCurrentChange(val) {
                 this.listQuery.page = val
                 this.getList()
+            },
+            showInfo(row){
+                var self = this
+                self.infoList = null
+                self.total = null
+                self.listQuery.agent_merchant_name = row.username
+                self.listQuery.agent_merchant_id = row.merchant_id
+                axios.post('/admin/report/account-open-fee-info', self.listQuery).then(
+                    res => {
+                        self.listLoading = false
+                        if(res.code != 0 ){
+                            self.$message.error({message:res.message})
+                        }else{
+                            self.infoVisible = true
+                            self.infoList = res.data.list
+                            self.total = res.data.total
+                        }
+                    }
+                )
             },
         }
     }
